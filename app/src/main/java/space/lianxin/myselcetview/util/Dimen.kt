402cb3113f4c8e5a22a9f15958da74c9d33b01ef
lianxin <lianxin@aimymusic.com>
2020-06-11 18:35:45 +0800

    UI界面优化。

---
+++ app/src/main/java/space/lianxin/myselcetview/util/Dimen.kt
@@ -0,0 +1,17 @@
+package space.lianxin.myselcetview.util
+
+import android.content.Context
+
+/**
+ * description:
+ * @date: 2020/6/11 12:25
+ * @author: lianxin
+ */
+fun Float.dp2px(context: Context): Float = this * context.resources.displayMetrics.density
+
+fun Int.dp2px(context: Context): Float = this * context.resources.displayMetrics.density
+
+
+//fun dp2px(context: Context, dp: Float): Float = dp * context.resources.displayMetrics.density
+
+//fun px2dp(context: Context, px: Float): Float = px / context.resources.displayMetrics.density
--- app/src/main/java/space/lianxin/myselcetview/view/SelectViewT.kt
+++ app/src/main/java/space/lianxin/myselcetview/view/SelectViewT.kt
@@ -2,24 +2,69 @@ package space.lianxin.myselcetview.view
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.Rect
+import android.graphics.*
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
 import android.view.MotionEvent
+import space.lianxin.myselcetview.R
+import space.lianxin.myselcetview.util.dp2px
 
 /**
  * description:右侧快速索引（onDraw实现）
- * 弹窗和文字占比2:1
  * @date: 2020/6/9 22:13
  * @author: lianxin
  */
-class SelectViewT constructor(context: Context?, attrs: AttributeSet?, defStyle: Int) :
+class SelectViewT @JvmOverloads constructor(context: Context?, attrs: AttributeSet? = null, defStyle: Int = 0) :
   View(context, attrs, defStyle) {
 
-  constructor(context: Context?, attrs: AttributeSet?) : this(context, attrs, 0) {
+  // 绘制View画笔
+  private var paint: Paint = Paint()
+  // 默认条目颜色
+  private val defaultColor = Color.rgb(153, 153, 153)
+  // 选中条目颜色
+  private val selectColor = Color.rgb(48, 209, 139)
+  // 弹出文字颜色
+  private val popColor = Color.rgb(255, 255, 255)
+
+  // 内置上边距,单位dp
+  private val heightMarginTop: Int = 30
+  // 内置下边距,单位dp
+  private val heightMarginBottom: Int = 30
+
+  // 条目宽度
+  private var viewWidth: Int = 0
+  // 文字宽度，包括边距，单位：dp
+  private val wordWidth: Int = 16
+  // 条目高度
+  private var viewHeight: Int = 0
+  // 文字高度，包括边距，单位：dp
+  private val wordHeight: Int = 16
+  // 弹出图片与文字的横向距离，单位：dp
+  private val imgMargin: Float = 19.5f
+  // 文字纵向距离，单位dp
+  private val wordMargin: Float = 1f
+  // 弹出图片宽度,单位：dp
+  private val imgWidth: Int = 48
+  // 弹出图片高度,单位：dp
+  private val imgHeight: Int = 48
+  // 绘制的文字大小,单位：px
+  private val wordSize: Float = 11.dp2px(context!!)
+
+  // 索引值
+  private var items = arrayListOf(
+    "A", "B", "C", "D", "E", "F", "G",
+    "H", "I", "J", "K", "L", "M", "N",
+    "O", "P", "Q", "R", "S", "T",
+    "U", "V", "W", "X", "Y", "Z", "#"
+  )
+  // 当前选中索引下标，默认未选中任何索引
+  private var selectIndex: Int = -1
+
+  // 设置索引改变监听
+  private var onSelectIndexChangeListener: OnSelectIndexChangeListener? = null
+
+  init {
     // 初始化画笔
     paint = Paint()
     // 设置画笔颜色
@@ -28,37 +73,6 @@ class SelectViewT constructor(context: Context?, attrs: AttributeSet?, defStyle:
     paint.isAntiAlias = true
   }
 
-  constructor(context: Context?) : this(context, null)
-
-  // 绘制View画笔
-  private var paint: Paint = Paint()
-  // 默认条目颜色
-  private val defaultColor = Color.BLUE
-  // 选中条目颜色
-  private val selectColor = Color.RED
-  // 圆背景色
-  private val circleColor = Color.RED
-  // 圆上字的颜色
-  private val circlrTextColor = Color.WHITE
-
-  // 条目宽度
-  private var itemWidth: Int = 0
-  // 条目高度
-  private var itemHeight: Int = 0
-
-  // 索引值
-  private var items = arrayListOf(
-    "A", "B", "C", "D", "E", "F", "G",
-    "H", "I", "J", "K", "L", "M", "N",
-    "O", "P", "Q", "R", "S", "T",
-    "U", "V", "W", "X", "Y", "Z"
-  )
-  // 当前选中索引下标，默认未选中任何索引
-  private var selectIndex: Int = -1
-
-  // 设置索引改变监听
-  private var onSelectIndexChangeListener: OnSelectIndexChangeListener? = null
-
   /**
    * 视图测量，根据测量结果，算出每个item需要的宽高。
    */
@@ -66,12 +80,16 @@ class SelectViewT constructor(context: Context?, attrs: AttributeSet?, defStyle:
     super.onMeasure(widthMeasureSpec, heightMeasureSpec)
 
     // 计算条目所需要的宽度
-//    itemWidth = MeasureSpec.getSize(widthMeasureSpec)
-    itemWidth = measuredWidth
-
+    viewWidth = (wordWidth + imgMargin + imgWidth).dp2px(context).toInt()
     // 计算条目所需要的高度
-//    itemHeight = MeasureSpec.getSize(heightMeasureSpec) / items.size
-    itemHeight = measuredHeight / items.size
+    viewHeight = ((wordHeight + wordMargin) * items.size - wordMargin + heightMarginTop + heightMarginBottom).dp2px(context).toInt()
+    Log.d("qingyi", "onMeasure: viewWidth=${viewWidth},viewHeight=${viewHeight}")
+
+    MeasureSpec.makeMeasureSpec(viewWidth, MeasureSpec.getMode(widthMeasureSpec))
+    MeasureSpec.makeMeasureSpec(viewHeight, MeasureSpec.getMode(heightMeasureSpec))
+    setMeasuredDimension(viewWidth, viewHeight)
+
+
 
   }
 
@@ -89,26 +107,33 @@ class SelectViewT constructor(context: Context?, attrs: AttributeSet?, defStyle:
       // 判断下标命中,选择画笔颜色
       if (selectIndex == i) {
         paint.color = selectColor
-        // 计算圆心位置,半径
-        val cx = itemWidth / 3f
-        val cy = (itemHeight * selectIndex + (itemHeight / 2)).toFloat()
-        val r = itemWidth * 0.3f
-        drawCircle(canvas, cx, cy, r)
+        // 画弹窗
+        drawPop(canvas)
       } else {
         paint.color = defaultColor
       }
 
       // 读取条目文字，开始绘制
       val word = items[i]
-      paint.textSize = 40f
+      paint.textSize = wordSize
       paint.getTextBounds(word, 0, 1, rect)
       // 文字宽高
-      val wordWidth = rect.width()
-      val wordHeight = rect.height()
+      val ww = rect.width()
+      val wh = rect.height()
+      Log.d("qingyi", "ondraw绘制第${i}个，ww=$ww,wh=$wh")
       // 计算文字出现位置并绘制
-      val wordX = itemWidth * 0.835f - (wordWidth / 2)
-      val wordY = (itemHeight / 2 + wordHeight / 2 + i * itemHeight).toFloat()
+      val wordX = viewWidth - ((wordWidth.dp2px(context) + ww) / 2)
+      val wordY = (heightMarginTop + (i * (wordHeight + wordMargin))).dp2px(context) + ((wordHeight.dp2px(context) + wh) / 2)
+      Log.d("qingyi", "onDraw: 绘制第${i}个，wordX=$wordX,wordY=$wordY")
       canvas?.drawText(word, wordX, wordY, paint)
+
+      // 使用Paint.Paint.Align.CENTER简化文字左右对齐方式，简化问题。
+      // 但是还是需要计算Y轴中值点。
+//      paint.textAlign = Paint.Align.CENTER
+//      val wordX = viewWidth - (wordWidth.dp2px(context) / 2)
+//      val wordY = (heightMarginTop + (i * (wordHeight + wordMargin))).dp2px(context) + ((wordHeight.dp2px(context)) / 2)
+//      Log.d("qingyi", "onDraw: 绘制第${i}个，wordX=$wordX,wordY=$wordY")
+//      canvas?.drawText(word, wordX, wordY, paint)
     }
 
   }
@@ -121,7 +146,8 @@ class SelectViewT constructor(context: Context?, attrs: AttributeSet?, defStyle:
     when (event?.action) {
       MotionEvent.ACTION_DOWN, MotionEvent.ACTION_MOVE -> {
         // 根据Y轴坐标计算选中的索引值
-        val index = (event.y / itemHeight).toInt()
+        val index = ((event.y - heightMarginTop.dp2px(context)) / (wordHeight + wordMargin).dp2px(context)).toInt()
+        Log.d("qingyi", "onTouchEvent: event.y=${event.y}")
         // 判断索引更改
         if (index != selectIndex) {
           // 更改索引下标，并重绘
@@ -145,26 +171,31 @@ class SelectViewT constructor(context: Context?, attrs: AttributeSet?, defStyle:
     return true
   }
 
+  // 图片资源
+  private var bitmap = BitmapFactory.decodeResource(resources, R.mipmap.float_alphabet)
 
-  private fun drawCircle(canvas: Canvas?, cx: Float, cy: Float, r: Float) {
+  private fun drawPop(canvas: Canvas?) {
     // 保存画笔颜色
-    val saveColor = paint.color
-    paint.color = circleColor
+    val c = paint.color
 
-    // 画实心圆
-    paint.style = Paint.Style.FILL
-    canvas?.drawCircle(cx, cy, r, paint)
-
-    // 画实心圆上的文字
-    paint.color = circlrTextColor
-    paint.textSize = 60f
+    // 绘制图片,left:图片左顶点x轴坐标；top:图片左顶点y轴坐标
+    val left = 0f
+    val top = (heightMarginTop + selectIndex * (wordHeight + wordMargin) + wordHeight / 2 - imgHeight / 2).dp2px(context)
+    canvas?.drawBitmap(bitmap, left, top, paint)
+    // 绘制文字
+    paint.textSize = 20.dp2px(context)
+    paint.color = popColor
+    // 根据图片左顶点计算坐标。
     paint.getTextBounds(items[selectIndex], 0, 1, rect)
-    // 计算文字位置
-    val wordX = cx - (rect.width() / 2)
-    val wordY = cy + (rect.height() / 2)
+    val vx = rect.width()
+    val vy = rect.height()
+    // 文字位置:文在在图中左右比例2:3，垂直居中
+    val wordX = left + (imgWidth.dp2px(context) - vx) * 0.4f
+    val wordY = top + ((imgHeight.dp2px(context) + vy) / 2)
     canvas?.drawText(items[selectIndex], wordX, wordY, paint)
-    // 还原画笔颜色
-    paint.color = saveColor
+
+    //还原画笔颜色
+    paint.color = c
   }
 
   /**
